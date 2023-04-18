99cca1e894d551388c520663f7afd4a7a0ec5644
Dan Berkowitz <dansberkowitz@gmail.com>
2023-04-18 00:03:41 -0400

    Im not proud of what I have done

--- build.sh
+++ build.sh
@@ -8,11 +8,24 @@ for fn in `cat build_versions.txt`; do
   echo "project.ext.jooqVersion=\"$ver2\"" >> versions.gradle;
 
   if [ "${ver1:0:1}" = "4" ]; then
+    # Dropwizard 4 needs jakarta as the namespace, and jooq 3.18.3 needs java 17
     find ./dropwizard-jooq/src/main/java -type f -exec sed -i "s/javax.ws.rs.ext.Provider/jakarta.ws.rs.ext.Provider/g" {} +
-    # find ./dropwizard-jooq/src/main/java -type f -exec sed -i "s/javax.ws.rs.core.Feature/jakarta.ws.rs.core.Feature/g" {} +
     find ./drop* -type f -exec sed -i "s/javax.validation.constraints.NotNull/jakarta.validation.constraints.NotNull/g" {} +
     find ./drop* -type f -exec sed -i "s/javax.ws.rs./jakarta.ws.rs./g" {} +
     find ./dropwizard-jooq/src/main/java -type f -exec sed -i "s/javax.inject.Singleton/jakarta.inject.Singleton/g" {} +
+  elif
+    find ./dropwizard-jooq/src/main/java -type f -exec sed -i "s/jakarta.ws.rs.ext.Provider/javax.ws.rs.ext.Provider/g" {} +
+    find ./drop* -type f -exec sed -i "s/jakarta.validation.constraints.NotNull/javax.validation.constraints.NotNull/g" {} +
+    find ./drop* -type f -exec sed -i "s/jakarta.ws.rs./javax.ws.rs./g" {} +
+    find ./dropwizard-jooq/src/main/java -type f -exec sed -i "s/jakarta.inject.Singleton/javax.inject.Singleton/g" {} +
+  fi
+
+  if [ "${ver2:2:2}" = "18" ]; then
+    sed -i "s/sourceCompatibility = \"11\"/sourceCompatibility = \"17\"/g" build.gradle
+    sed -i "s/targetCompatibility = \"11\"/targetCompatibility = \"17\"/g" build.gradle
+  elif
+    sed -i "s/sourceCompatibility = \"17\"/sourceCompatibility = \"11\"/g" build.gradle
+    sed -i "s/targetCompatibility = \"17\"/targetCompatibility = \"11\"/g" build.gradle
   fi
 
   ./gradlew clean generatejooq build
